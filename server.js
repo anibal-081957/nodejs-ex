35281be0aa5deb04ed2c4ab0aeee644622feb4af
anibal-081957 <sergio.anibal.per@gmail.com>
2017-03-30 13:56:40 -0300

    agregado pagina datos

--- server.js
+++ server.js
@@ -76,6 +76,28 @@ app.get('/', function (req, res) {
   }
 });
 
+//Codigo insertado por mi
+app.get('/datos', function (req, res) {
+
+  /*
+    if (!db) {
+        initDb(function(err){});
+    }
+    if (db) {
+        var col = db.collection('counts');
+        // Create a document with request IP and current time of request
+        col.insert({ip: req.ip, date: Date.now()});
+        col.count(function(err, count){
+            res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
+        });
+    } else {
+        res.render('index.html', { pageCountMessage : null});
+    }
+    */
+  res.render('datos.html', { datos: "esto es una prueba"});
+});
+//fin de mi codigo
+
 app.get('/pagecount', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
